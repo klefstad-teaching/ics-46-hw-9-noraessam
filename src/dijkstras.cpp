b877bad65ea51ac03c371aa68088c5b416f54fb2
noraessam <nora.bukhaseen@gmail.com>
2025-03-11 08:15:36 +0000

    first

---
+++ src/dijkstras.cpp
@@ -0,0 +1,71 @@
+#include "dijkstras.h"
+#include <algorithm>
+
+struct Node {
+    int vertex;
+    int distance;
+    Node(int v, int d) : vertex(v), distance(d) {}
+};
+
+struct CompareNode {
+    bool operator()(const Node& n1, const Node& n2) const {
+        return n1.distance > n2.distance;
+    }
+};
+
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+    int n = G.numVertices;
+    vector<int> distance(n, INF);
+    previous.assign(n, -1);
+    vector<bool> visited(n, false);
+
+    distance[source] = 0;
+    priority_queue<Node, vector<Node>, CompareNode> pq;
+    pq.push(Node(source, 0));
+
+    while (!pq.empty()) {
+        Node current = pq.top();
+        pq.pop();
+        int u = current.vertex;
+
+        if (visited[u])
+            continue;
+        visited[u] = true;
+
+        for (const Edge& edge : G[u]) {
+            int v = edge.dst;
+            int weight = edge.weight;
+            if (!visited[v] && distance[u] + weight < distance[v]) {
+                distance[v] = distance[u] + weight;
+                previous[v] = u;
+                pq.push(Node(v, distance[v]));
+            }
+        }
+    }
+    return distance;
+}
+
+vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
+    vector<int> path;
+    
+    if (distances[destination] == INF)
+        return path;
+    
+    int current = destination;
+    while (current != -1) {
+        path.push_back(current);
+        current = previous[current];
+    }
+    reverse(path.begin(), path.end());
+    return path;
+}
+
+void print_path(const vector<int>& v, int total) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        cout << v[i];
+        if (i < v.size() - 1)
+            cout << " ";
+    }
+    cout << endl;
+    cout << "Total cost is " << total << endl;
+}
---
+++ src/ladder_main.cpp
@@ -0,0 +1,13 @@
+#include "ladder.h"
+#include <iostream>
+#include <string>
+#include <set>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    verify_word_ladder();
+    
+    return 0;
+}
